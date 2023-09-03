cae5674db2bdfb3b363475e92978693812e149a8
Angelika <angelika.holod@gmail.com>
2023-09-03 21:31:19 +0200

    railway api

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'tGK5QsjHWnOHqitBaIwV',
+        'HOST': 'containers-us-west-196.railway.app',
+        'PORT': '7412',
     }
 }
 
